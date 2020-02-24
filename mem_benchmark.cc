d9a2747ef2d6f41504fae296beb98403259964b3
sebastian <sebsimmon@reed.edu>
2020-02-24 03:00:05 +0000

    Initial commit

---
+++ mem_benchmark.cc
@@ -0,0 +1,13 @@
+//mem_benchmark.cc - Author: Sebastian Simmons
+#include <stdlib.h>
+#include <iostream>
+
+
+
+int main(int argc, char *argv[]){
+	int base = 2;
+	for(int i =10; i <= 26; i++){
+		std::cout << 2**i;
+	}
+
+}
