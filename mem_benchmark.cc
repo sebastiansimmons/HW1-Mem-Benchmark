6a13cc7423452a12f0f4a336e026017ed7378851
sebastian <sebsimmon@reed.edu>
2020-02-24 06:27:25 +0000

    Updated mem_benchmark.cc

--- mem_benchmark.cc
+++ mem_benchmark.cc
@@ -1,13 +1,33 @@
 //mem_benchmark.cc - Author: Sebastian Simmons
 #include <stdlib.h>
 #include <iostream>
+#include <chrono>
+#include <math.h>
+#include <thread>
+#include <cstring>
+#include <sstream>
 
+void test_buffer(int size){
+	char * buffer = new char [size];
+	//std::iostream stream (buffer,);
+	std::istringstream input(buffer);
+	auto start = std::chrono::high_resolution_clock::now(); 
+
+	input.readsome(nullptr, size);
+    
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto t = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
+	std::cout << size << "	" << t.count() << std::endl;
+	return;
+}
 
 
 int main(int argc, char *argv[]){
-	int base = 2;
-	for(int i =10; i <= 26; i++){
-		std::cout << 2**i;
-	}
+	std::cout << "# Bytes    time" << std::endl;
+	//test_buffer(1024);
 
+	for(int i =10; i <= 26; i++){
+		test_buffer(pow(2,i));
+	}
+	return 0;
 }
